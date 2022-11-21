bad5e35db35f8bdd1f9174f19cdaaeffb2dfda53
DenisRazvorotnikov <deniska_raz26@mail.ru>
2022-11-21 22:55:55 +0300

    Add of folders

---
+++ PY/numbers.py
@@ -0,0 +1,11 @@
+print("enter four digits: ")
+dg1 = int(input())
+dg2 = int(input())
+dg3 = int(input())
+dg4 = int(input())
+
+sum1 = dg1 + dg2
+sum2 = dg3 + dg4
+div = sum1 / sum2
+
+print("we have made some calculation\nthe result is: ",format(div,".3"))
